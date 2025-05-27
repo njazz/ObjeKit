2089b2e85e3e96e90a7dfc43807ff1fb4b48ef03
Alex <anadjarov@gmail.com>
2025-05-27 23:11:16 +0200

    examples added

---
+++ Examples/ObjeKitTest.swift
@@ -0,0 +1,74 @@
+//
+//  MaxSDKKitTest.swift
+//  max-sdk-kit-test
+//
+//  Created by alex on 25/05/2025.
+//
+
+import ObjeKit
+
+import Cocoa
+import SwiftUI
+
+class ObjeKitTest : MaxObject {
+    static var className: String { "ObjeKitTest"}
+    
+    required init() {
+        MaxRuntime.post("test object: init")
+    }
+    
+    deinit {
+        MaxRuntime.post("test object: deinit")
+    }
+    
+    @Inlet(2)
+    var inlet2 : Int = 10
+    
+    @MaxState
+    var value : Float = 3.3
+    
+    @MaxIOBuilder
+    var io: any MaxIOComponent {
+        
+        Inlet() { (v:Int) in self.inlet2 = v }
+        
+        MaxMethod() {
+            self.value += 1
+            MaxRuntime.post("bang! value: \(self.value)")
+        }
+        
+        MaxMethod(){ (value : Double) in
+            MaxRuntime.post("float: \(value)")
+        }
+        
+        MaxMethod(){ (value : CLong) in
+            MaxRuntime.post("int: \(value)")
+        }
+                
+        MaxMethod("test") { value in
+            self.inlet2 = 0
+            MaxRuntime.post("test: \(value)")
+        }
+        
+        MaxMethod() { (value:[MaxValue]) in
+            MaxRuntime.post("list: \(value)")
+        }
+
+        Outlet(0) { self.$value }
+        Outlet { self.$value }
+    }
+    
+//    @MaxObjectBuilder
+//    var objects: any MaxObject {
+//        Test()
+//    }
+    
+}
+
+
+@_cdecl("ext_main")
+public func ext_main(_ r: UnsafeMutableRawPointer) {
+    MaxDispatcher.setup(ObjeKitTest.self)
+}
+
+
