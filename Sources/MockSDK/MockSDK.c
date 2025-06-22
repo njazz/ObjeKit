dcf3d0224a2a54d5d61947f0ef1259f8dff7b30b
Alex <anadjarov@gmail.com>
2025-06-22 17:57:50 +0200

    fix for the tests

--- Sources/MockSDK/MockSDK.c
+++ Sources/MockSDK/MockSDK.c
@@ -46,3 +46,12 @@ void class_add_attr();
 t_object *attr_offset_new(C74_CONST char *name, C74_CONST t_symbol *type, long flags, C74_CONST method mget, C74_CONST method mset, long offset){ return 0; }
 
 t_max_err class_addattr(t_class *c,t_object *attr) { return 0; }
+
+t_max_err class_attr_addattr_format(t_class *c, C74_CONST char *attrname, C74_CONST char *attrname2, C74_CONST t_symbol *type, long flags, C74_CONST char *fmt, ...){}
+t_max_err class_attr_addattr_parse(t_class *c, C74_CONST char *attrname, C74_CONST char *attrname2, t_symbol *type, long flags, C74_CONST char *parsestr){}
+void *class_attr_get(t_class *x, t_symbol *attrname){ return 0; }
+
+t_symbol *gensym_tr(const char *s) { return 0;}
+
+void *object_method_imp(void *x, void *sym, void *p1, void *p2, void *p3, void *p4, void *p5, void *p6, void *p7, void *p8) {}
+
